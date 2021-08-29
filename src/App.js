f3a7464e7e90018568402eac02b33167f2001322
Taha-boudiaf <67803030+Taha-boudiaf@users.noreply.github.com>
2021-08-29 00:23:04 -0700

    install eslint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import "./App.css";
+import './App.css'
 
 function App() {
-  return (
-    <div className='App'>
-      <p>test</p> <p>test</p>
-    </div>
-  );
+    return (
+        <div className="App">
+            <p>test</p>
+        </div>
+    )
 }
 
-export default App;
+export default App
