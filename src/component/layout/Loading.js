e1bb2a5352bd36a6a2c32e856a5da9a1683e8c51
Taha-boudiaf <67803030+Taha-boudiaf@users.noreply.github.com>
2021-08-30 06:26:49 -0700

    edit style loading

--- src/component/layout/Loading.js
+++ src/component/layout/Loading.js
@@ -2,10 +2,14 @@ import React from 'react'
 import load from '../../assets/loading.gif'
 const Loading = () => {
   return (
-    <div>
+    <div style={loadStyle}>
       <img src={load} alt="loading..." />
     </div>
   )
 }
 
+const loadStyle = {
+  display: 'flex',
+  justifyContent: 'center'
+}
 export default Loading
