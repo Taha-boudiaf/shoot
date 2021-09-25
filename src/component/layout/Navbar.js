22968650108db90a333f0dcda2be11b71bfc0648
Taha-boudiaf <67803030+Taha-boudiaf@users.noreply.github.com>
2021-09-25 17:24:52 +0100

    ElevationScroll  bar

--- src/component/layout/Navbar.js
+++ src/component/layout/Navbar.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { cloneElement } from 'react'
 import { makeStyles, AppBar, Toolbar } from '@material-ui/core'
 import { Link } from 'react-router-dom'
+import CssBaseline from '@mui/material/CssBaseline'
+import useScrollTrigger from '@mui/material/useScrollTrigger'
+import PropTypes from 'prop-types'
+
 import Logo from '../icon/Logo'
 
 const useStyles = makeStyles((theme) => ({
@@ -13,21 +17,40 @@ const useStyles = makeStyles((theme) => ({
   }
 }))
 
-const Navbar = () => {
+function ElevationScroll(props) {
+  const { children, window } = props
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined
+  })
+
+  return cloneElement(children, {
+    elevation: trigger ? 4 : 0
+  })
+}
+const Navbar = (props) => {
   const classes = useStyles()
 
   return (
     <>
-      <AppBar position="static">
-        <Toolbar className={classes.toolBar}>
-          <Link to="/">
-            <Logo />
-          </Link>
-          <Link to="about">About</Link>
-        </Toolbar>
-      </AppBar>
+      <CssBaseline />
+      <ElevationScroll {...props}>
+        <AppBar>
+          <Toolbar className={classes.toolBar}>
+            <Link to="/">
+              <Logo />
+            </Link>
+            <Link to="about">About</Link>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <Toolbar />
     </>
   )
 }
-
+ElevationScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  window: PropTypes.func
+}
 export default Navbar
